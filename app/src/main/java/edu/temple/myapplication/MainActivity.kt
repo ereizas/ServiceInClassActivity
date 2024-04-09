be7d9104d594e6667749e24141eb5ad9b08319fc
Eric Reizas <tuo17224@temple.edu>
2024-04-09 11:31:00 -0400

    Added and implemented ServiceConnection obj

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -9,13 +9,25 @@ import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var timerBinder : TimerService.TimerBinder
+    var isConnected = false
+    val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected=true
+        }
 
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected=false
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindService(
             Intent(this, TimerService::class.java),
-            ,
+            serviceConnection,
             BIND_AUTO_CREATE
         )
         findViewById<Button>(R.id.startButton).setOnClickListener {
