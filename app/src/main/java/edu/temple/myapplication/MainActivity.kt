5c443ce4ffd1a6071b04c825dd36a73a6467a5ff
Eric Reizas <tuo17224@temple.edu>
2024-04-09 11:29:12 -0400

    Added call to bindService

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,7 +1,11 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        bindService(
+            Intent(this, TimerService::class.java),
+            ,
+            BIND_AUTO_CREATE
+        )
         findViewById<Button>(R.id.startButton).setOnClickListener {
 
         }
